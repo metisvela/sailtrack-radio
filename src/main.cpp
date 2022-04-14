0be314e401b4e5d9fca3f45c0be3b86dbd5b9152
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2022-04-14 16:05:28 +0200

    Refactor + LoRa initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,31 @@
 #include <Arduino.h>
-#include <ArduinoJson.h>
+#include <SailtrackModule.h>
 #include <axp20x.h>
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h>
-#include <SailtrackModule.h>
+#include <LoRa.h>
 
-#define I2C_SDA 21
-#define I2C_SCL 22
+#define NOTIFICATION_LED_PIN 4
 
 #define GPS_RX_PIN 34
 #define GPS_TX_PIN 12
-#define GPS_BAND_RATE 9600
+#define GPS_BAUD_RATE 9600
 
-SFE_UBLOX_GNSS GPS;
-AXP20X_Class PMU;
-SailtrackModule STM;
+#define LORA_FREQUENCY 868E6
+
+#define GPS_UPDATE_RATE_HZ 20
+#define MQTT_PUBLISH_RATE_HZ 5
+
+SFE_UBLOX_GNSS gps;
+AXP20X_Class pmu;
+SailtrackModule stm;
 
 class ModuleCallbacks: public SailtrackModuleCallbacks {
-	
 	DynamicJsonDocument * getStatus() {
 		DynamicJsonDocument * payload =  new DynamicJsonDocument(300);
 		JsonObject battery = payload->createNestedObject("battery");
 		JsonObject cpu = payload->createNestedObject("cpu");
-		battery["voltage"] = PMU.getBattVoltage() / 1000;
-		battery["charging"] = PMU.isChargeing();
+		battery["voltage"] = pmu.getBattVoltage() / 1000;
+		battery["charging"] = pmu.isChargeing();
 		cpu["temperature"] = temperatureRead();
 		return payload;
 	}
@@ -38,44 +41,44 @@ void onGPSData(UBX_NAV_PVT_data_t ubxDataStruct) {
 	payload["hacc"] = ubxDataStruct.hAcc;
 	payload["sacc"] = ubxDataStruct.sAcc;
 	payload["headacc"] = ubxDataStruct.headAcc;
-	STM.publish("sensor/gps0", &payload);
+	payload["fix"] = ubxDataStruct.fixType;
+	stm.publish("sensor/gps0", &payload);
 }
 
 void beginPMU() {
-	Wire.begin(I2C_SDA, I2C_SCL);
-	PMU.begin(Wire, AXP192_SLAVE_ADDRESS);
-	PMU.setPowerOutPut(AXP192_DCDC2, AXP202_OFF);
-    PMU.setPowerOutPut(AXP192_LDO2, AXP202_OFF);
-    PMU.setPowerOutPut(AXP192_LDO3, AXP202_OFF);
-    PMU.setPowerOutPut(AXP192_EXTEN, AXP202_OFF);
+	Wire.begin(SDA, SCL);
+	pmu.begin(Wire, AXP192_SLAVE_ADDRESS);
+	pmu.setPowerOutPut(AXP192_DCDC2, AXP202_OFF);
+    pmu.setPowerOutPut(AXP192_EXTEN, AXP202_OFF);
 }
 
 void beginGPS() {
-	PMU.setLDO3Voltage(3300);
-	PMU.setPowerOutPut(AXP192_LDO3, AXP202_ON);
-	Serial1.begin(GPS_BAND_RATE, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
-	GPS.begin(Serial1);
-	GPS.setUART1Output(COM_TYPE_UBX);
-	GPS.setMeasurementRate(200);
-	GPS.setAutoPVTcallback(&onGPSData);
+	pmu.setLDO3Voltage(3300);
+	pmu.setPowerOutPut(AXP192_LDO3, AXP202_ON);
+	Serial1.begin(GPS_BAUD_RATE, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+	gps.begin(Serial1);
+	gps.setUART1Output(COM_TYPE_UBX);
+	gps.setMeasurementRate(1000 / MQTT_PUBLISH_RATE_HZ);
+	gps.setAutoPVTcallback(&onGPSData);
 }
 
 void beginLora() {
-	PMU.setLDO2Voltage(3300);
-	PMU.setPowerOutPut(AXP192_LDO2, AXP202_ON);
-	// TODO: Init LoRa
+	pmu.setLDO2Voltage(3300);
+	pmu.setPowerOutPut(AXP192_LDO2, AXP202_ON);
+	LoRa.setPins(LORA_CS, LORA_RST, LORA_IO0);
+	LoRa.begin(LORA_FREQUENCY);
 }
 
 void setup() {
 	beginPMU();
-	STM.setNotificationLed(4,true);
-	STM.begin("radio", IPAddress(192, 168, 42, 101), new ModuleCallbacks());
+	stm.setNotificationLed(NOTIFICATION_LED_PIN, true);
+	stm.begin("radio", IPAddress(192, 168, 42, 101), new ModuleCallbacks());
 	beginGPS();
-	//beginLora();
+	beginLora();
 }
 
 void loop() {
-	GPS.checkUblox();
-	GPS.checkCallbacks();
-	delay(50);
+	gps.checkUblox();
+	gps.checkCallbacks();
+	delay(1000 / GPS_UPDATE_RATE_HZ);
 }
