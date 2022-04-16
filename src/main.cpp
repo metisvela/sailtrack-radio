7dd823c0f9d325828c478db4326cc50e33304779
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2022-04-16 18:47:52 +0200

    Add lora initialization

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,33 @@
 #include <SailtrackModule.h>
 #include <axp20x.h>
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h>
-#include <LoRa.h>
+#include <RadioLib.h>
 
 #define NOTIFICATION_LED_PIN 4
 
+#define GPS_BAUD_RATE 9600
+#define GPS_SERIAL_CONFIG SERIAL_8N1
 #define GPS_RX_PIN 34
 #define GPS_TX_PIN 12
-#define GPS_BAUD_RATE 9600
-
-#define LORA_FREQUENCY 868E6
-
 #define GPS_UPDATE_RATE_HZ 20
+
+#define LORA_CS_PIN 18
+#define LORA_DIO1_PIN 33
+#define LORA_RST_PIN 23
+#define LORA_BUSY_PIN 32
+
+// EBYTE E32-868T20D parameters
+#define LORA_FREQUENCY_MHZ 868
+#define LORA_BANDWIDTH_KHZ 500
+#define LORA_SPREADING_FACTOR 11
+#define LORA_CODING_RATE_DENOM 5
+
 #define MQTT_PUBLISH_RATE_HZ 5
 
 SFE_UBLOX_GNSS gps;
 AXP20X_Class pmu;
 SailtrackModule stm;
+SX1262 lora = new Module(LORA_CS_PIN, LORA_DIO1_PIN, LORA_RST_PIN, LORA_BUSY_PIN);
 
 class ModuleCallbacks: public SailtrackModuleCallbacks {
 	DynamicJsonDocument * getStatus() {
@@ -37,25 +48,24 @@ void onGPSData(UBX_NAV_PVT_data_t ubxDataStruct) {
 	payload["longitude"] = ubxDataStruct.lon;
 	payload["speed"] = ubxDataStruct.gSpeed;
 	payload["heading"] = ubxDataStruct.headMot;
-	payload["vacc"] = ubxDataStruct.vAcc;
-	payload["hacc"] = ubxDataStruct.hAcc;
-	payload["sacc"] = ubxDataStruct.sAcc;
-	payload["headacc"] = ubxDataStruct.headAcc;
 	payload["fix"] = ubxDataStruct.fixType;
 	stm.publish("sensor/gps0", &payload);
 }
 
 void beginPMU() {
-	Wire.begin(SDA, SCL);
+	Wire.begin();
 	pmu.begin(Wire, AXP192_SLAVE_ADDRESS);
+	pmu.setPowerOutPut(AXP192_DCDC1, AXP202_OFF);
 	pmu.setPowerOutPut(AXP192_DCDC2, AXP202_OFF);
+	pmu.setPowerOutPut(AXP192_LDO2, AXP202_OFF);
+	pmu.setPowerOutPut(AXP192_LDO3, AXP202_OFF);
     pmu.setPowerOutPut(AXP192_EXTEN, AXP202_OFF);
 }
 
 void beginGPS() {
 	pmu.setLDO3Voltage(3300);
 	pmu.setPowerOutPut(AXP192_LDO3, AXP202_ON);
-	Serial1.begin(GPS_BAUD_RATE, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+	Serial1.begin(GPS_BAUD_RATE, GPS_SERIAL_CONFIG, GPS_RX_PIN, GPS_TX_PIN);
 	gps.begin(Serial1);
 	gps.setUART1Output(COM_TYPE_UBX);
 	gps.setMeasurementRate(1000 / MQTT_PUBLISH_RATE_HZ);
@@ -65,8 +75,7 @@ void beginGPS() {
 void beginLora() {
 	pmu.setLDO2Voltage(3300);
 	pmu.setPowerOutPut(AXP192_LDO2, AXP202_ON);
-	LoRa.setPins(LORA_CS, LORA_RST, LORA_IO0);
-	LoRa.begin(LORA_FREQUENCY);
+	lora.begin(LORA_FREQUENCY_MHZ, LORA_BANDWIDTH_KHZ, LORA_SPREADING_FACTOR, LORA_CODING_RATE_DENOM);
 }
 
 void setup() {
