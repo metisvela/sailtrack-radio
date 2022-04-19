44c9204f465075b7d5efd5977d98dba58503a02c
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2022-04-20 00:37:46 +0200

    Changes:
    Add E32-868T20D translator
    Add keydump program
    Add first working lora communication feature

---
+++ src/keydump.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include <axp20x.h>
+#include <RadioLib.h>
+#include <E32-868T20D.h>
+
+#define LORA_CS_PIN 18
+#define LORA_DIO1_PIN 33
+#define LORA_RST_PIN 23
+#define LORA_BUSY_PIN 32
+
+// EBYTE E32-868T20D parameters
+#define E32_CHANNEL 0x09
+#define E32_ADDRESS 0x1310
+#define E32_BASE_FREQUENCY_MHZ 862
+#define E32_BANDWIDTH_KHZ 500
+#define E32_SPREADING_FACTOR 11
+#define E32_CODING_RATE_DENOM 5
+
+AXP20X_Class pmu;
+SX1262 lora = new Module(LORA_CS_PIN, LORA_DIO1_PIN, LORA_RST_PIN, LORA_BUSY_PIN);
+E32_868T20D e32;
+
+void beginPMU() {
+	Wire.begin();
+	pmu.begin(Wire, AXP192_SLAVE_ADDRESS);
+	pmu.setPowerOutPut(AXP192_DCDC1, AXP202_OFF);	// GPIO Pins Power Source
+	pmu.setPowerOutPut(AXP192_DCDC2, AXP202_OFF);	// Unused
+	pmu.setPowerOutPut(AXP192_LDO2, AXP202_OFF);	// LoRa Power Source
+	pmu.setPowerOutPut(AXP192_LDO3, AXP202_OFF);	// GPS Power Source
+    pmu.setPowerOutPut(AXP192_EXTEN, AXP202_OFF);	// External Connector Power Source
+}
+
+void beginLora() {
+	pmu.setLDO2Voltage(3300);
+	pmu.setPowerOutPut(AXP192_LDO2, AXP202_ON);
+	lora.begin(E32_BASE_FREQUENCY_MHZ + E32_CHANNEL, E32_BANDWIDTH_KHZ, E32_SPREADING_FACTOR, E32_CODING_RATE_DENOM);
+}
+
+void setup() {
+    Serial.begin(115200);
+    beginPMU();
+    beginLora();
+
+    Serial.println("Dumping keys...");
+    uint8_t packet[64];
+    size_t len;
+    for (int i = 0; i <= 0xFF; i++) {
+        e32.generateKeyDumpPacket(E32_ADDRESS, (uint8_t)i, packet, &len);
+        lora.transmit(packet, len);
+        Serial.print("Completed: ");
+        Serial.print(i+1); Serial.print("/"); Serial.println(256);
+        delay(100);
+    }
+    Serial.println("Done!");
+}
+
+void loop() {}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+#define USE_ESP_IDF_LOG
+
 #include <Arduino.h>
 #include <SailtrackModule.h>
 #include <axp20x.h>
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h>
 #include <RadioLib.h>
+#include <E32-868T20D.h>
 
 #define NOTIFICATION_LED_PIN 4
 
@@ -18,17 +21,35 @@
 #define LORA_BUSY_PIN 32
 
 // EBYTE E32-868T20D parameters
-#define LORA_FREQUENCY_MHZ 868
-#define LORA_BANDWIDTH_KHZ 500
-#define LORA_SPREADING_FACTOR 11
-#define LORA_CODING_RATE_DENOM 5
+#define E32_CHANNEL 0x09
+#define E32_ADDRESS 0x1310
+#define E32_BASE_FREQUENCY_MHZ 862
+#define E32_BANDWIDTH_KHZ 500
+#define E32_SPREADING_FACTOR 11
+#define E32_CODING_RATE_DENOM 5
 
-#define MQTT_PUBLISH_RATE_HZ 5
+#define MQTT_DATA_PUBLISH_RATE_HZ 1
+#define MQTT_LOG_PUBLISH_RATE_HZ 0.1
+#define LORA_SEND_RATE_HZ 0.7
+
+static const char * LOG_TAG = "SAILTRACK_RADIO";
+
+struct LoraObject {
+	char content[500];
+	char topic[20];
+	char name[20];
+} loraObjects[] = {
+	{ "{}", "sensor/gps0", "" },
+	{ "{}", "sensor/imu0", "orientation" }
+};
 
 SFE_UBLOX_GNSS gps;
 AXP20X_Class pmu;
 SailtrackModule stm;
 SX1262 lora = new Module(LORA_CS_PIN, LORA_DIO1_PIN, LORA_RST_PIN, LORA_BUSY_PIN);
+E32_868T20D e32;
+
+size_t loraSentBytes = 0;
 
 class ModuleCallbacks: public SailtrackModuleCallbacks {
 	DynamicJsonDocument * getStatus() {
@@ -39,6 +60,19 @@ class ModuleCallbacks: public SailtrackModuleCallbacks {
 		battery["charging"] = pmu.isChargeing();
 		cpu["temperature"] = temperatureRead();
 		return payload;
+	}
+
+	void onMqttMessage(const char * topic, const char * message) {
+		for (int i = 0; i < sizeof(loraObjects)/sizeof(*loraObjects); i++) {
+			LoraObject & object = loraObjects[i];
+			if (!strcmp(topic, object.topic)) {
+				if (strlen(object.name)) {
+					DynamicJsonDocument payload(500);
+					deserializeJson(payload, message);
+					serializeJson(payload[object.name], object.content);
+				} else strcpy(object.content, message);
+			}
+		}
 	}
 };
 
@@ -52,14 +86,49 @@ void onGPSData(UBX_NAV_PVT_data_t ubxDataStruct) {
 	stm.publish("sensor/gps0", &payload);
 }
 
+void loraTask(void * pvArguments) {
+	TickType_t lastWakeTime = xTaskGetTickCount();
+	while (true) {
+		for (auto object : loraObjects) {
+			char message[500];
+			sprintf(message, "%s%s\n", object.topic, object.content);
+			uint8_t packet[64];
+			size_t len;
+			size_t consumed = 0;
+			size_t toConsume = strlen(message);
+			while (consumed < toConsume) {
+				consumed += e32.encode(E32_ADDRESS, message + consumed, packet, &len);
+				lora.transmit(packet, len);
+				loraSentBytes += len;
+			}
+		}
+		vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(1000 / LORA_SEND_RATE_HZ));
+	}
+}
+
+void logTask(void * pvArguments) {
+	TickType_t lastWakeTime = xTaskGetTickCount();
+    while (true) {
+		float bw = loraSentBytes * 8 * MQTT_LOG_PUBLISH_RATE_HZ / 1000;
+        ESP_LOGI(LOG_TAG, "Used LoRa Bandwidth: %.2f kbit/s", bw);
+		loraSentBytes = 0;
+		vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(1000 / MQTT_LOG_PUBLISH_RATE_HZ));
+    }
+}
+
 void beginPMU() {
 	Wire.begin();
 	pmu.begin(Wire, AXP192_SLAVE_ADDRESS);
-	pmu.setPowerOutPut(AXP192_DCDC1, AXP202_OFF);
-	pmu.setPowerOutPut(AXP192_DCDC2, AXP202_OFF);
-	pmu.setPowerOutPut(AXP192_LDO2, AXP202_OFF);
-	pmu.setPowerOutPut(AXP192_LDO3, AXP202_OFF);
-    pmu.setPowerOutPut(AXP192_EXTEN, AXP202_OFF);
+	pmu.setPowerOutPut(AXP192_DCDC1, AXP202_OFF);	// GPIO Pins Power Source
+	pmu.setPowerOutPut(AXP192_DCDC2, AXP202_OFF);	// Unused
+	pmu.setPowerOutPut(AXP192_LDO2, AXP202_OFF);	// LoRa Power Source
+	pmu.setPowerOutPut(AXP192_LDO3, AXP202_OFF);	// GPS Power Source
+    pmu.setPowerOutPut(AXP192_EXTEN, AXP202_OFF);	// External Connector Power Source
+}
+
+void beginModule() {
+	stm.setNotificationLed(NOTIFICATION_LED_PIN, true);
+	stm.begin("radio", IPAddress(192, 168, 42, 101), new ModuleCallbacks());
 }
 
 void beginGPS() {
@@ -68,26 +137,35 @@ void beginGPS() {
 	Serial1.begin(GPS_BAUD_RATE, GPS_SERIAL_CONFIG, GPS_RX_PIN, GPS_TX_PIN);
 	gps.begin(Serial1);
 	gps.setUART1Output(COM_TYPE_UBX);
-	gps.setMeasurementRate(1000 / MQTT_PUBLISH_RATE_HZ);
+	gps.setMeasurementRate(1000 / MQTT_DATA_PUBLISH_RATE_HZ);
 	gps.setAutoPVTcallback(&onGPSData);
 }
 
 void beginLora() {
 	pmu.setLDO2Voltage(3300);
 	pmu.setPowerOutPut(AXP192_LDO2, AXP202_ON);
-	lora.begin(LORA_FREQUENCY_MHZ, LORA_BANDWIDTH_KHZ, LORA_SPREADING_FACTOR, LORA_CODING_RATE_DENOM);
+	lora.begin(E32_BASE_FREQUENCY_MHZ + E32_CHANNEL, E32_BANDWIDTH_KHZ, E32_SPREADING_FACTOR, E32_CODING_RATE_DENOM);
+	xTaskCreate(loraTask, "loraTask", TASK_MEDIUM_STACK_SIZE, NULL, TASK_MEDIUM_PRIORITY, NULL);
+}
+
+void beginLogging() {
+	esp_log_level_set(LOG_TAG, ESP_LOG_INFO);
+	xTaskCreate(logTask, "logTask", TASK_MEDIUM_STACK_SIZE, NULL, TASK_LOW_PRIORITY, NULL);
 }
 
 void setup() {
 	beginPMU();
-	stm.setNotificationLed(NOTIFICATION_LED_PIN, true);
-	stm.begin("radio", IPAddress(192, 168, 42, 101), new ModuleCallbacks());
+	beginModule();
 	beginGPS();
 	beginLora();
+	beginLogging();
+	for (auto object : loraObjects)
+		stm.subscribe(object.topic);
 }
 
+TickType_t lastWakeTime = xTaskGetTickCount();
 void loop() {
 	gps.checkUblox();
 	gps.checkCallbacks();
-	delay(1000 / GPS_UPDATE_RATE_HZ);
+	vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(1000 / GPS_UPDATE_RATE_HZ));
 }
